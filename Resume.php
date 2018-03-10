dd26d84dc12451a3e43fc206947543e0084e4ec3
10nagaraju407 <10nagaraju407@gmail.com>
2018-03-10 11:44:35 +0530

    Signed-off-by: 10nagaraju407 <10nagaraju407@gmail.com>

---
+++ Resume.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<title>Resume</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+		html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+		.w3-teal1, .w3-hover-teal1:hover {
+			
+			background-color: #ffffff!important;
+		}
+</style>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="images/phot1.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-red" style="text-align:right;">
+            <h2 >Nagaraju P</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Web Developer</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Hyderebad ,India</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>pnagaraju.info@gmail.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>7799514314</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
+          <p>Adobe Photoshop</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>Illustrator</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:85%">85%</div>
+          </div>
+          <p>HTML &amp CSS</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">80%</div>
+          </div> <p>JavaScript &amp JQurey</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b></p>
+          <p>Telugu </p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+          </div>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:75%"></div>
+          </div>
+          <p>Hindu</p>
+          <div class="w3-light-grey w3-round-xlarge ">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:25%  "></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
+        
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Web Developer /cenitithub.com </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Sep 2017 - Current</h6>
+          <p>Initially  i joined as intern cenitithub  designed  some web templates/layout by using Html and Css ,By learn Javascript/JQurey and AngulorJS later they recruted me as WebDeveloper. </p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Lab Assistant/ alits.ac.in/</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>July 2013 - Sep 2014</h6>
+          <p>I have practical work experienced  </p><br>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+       
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Sri Krishnadevaraya Engineering College </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2014 - 2017</h6>
+          <p>Master Degree</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>JNTU Anantapur</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
+          <p>Bachelor Degree</p><br>
+        </div>
+        
+       <div class="w3-container">
+          <h5 class="w3-opacity"><b>ESC Government Polytechnic</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2007 - 2010</h6>
+          <p>Diploma</p><br>
+        </div>
+        
+       <div class="w3-container">
+          <h5 class="w3-opacity"><b>APR School -Lepakshi </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2002 - 2007</h6>
+          <p>School</p><br>
+        </div>
+      </div>
+
+	  <hr>	  
+	  <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Hobbies</h2>
+       
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Playing Cricket </b></h5>
+		 
+		  <h5 class="w3-opacity"><b>Playing Shuttle </b></h5>
+		  <h5 class="w3-opacity"><b>Listing &amp Singing  songs</b></h5>
+		  <h5 class="w3-opacity"><b>Cooking</b></h5>
+		  <h5 class="w3-opacity"><b>Web Designing</b></h5>
+		  <hr>
+		  
+          
+        </div>
+        
+      </div>
+	  
+
+
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+   
+</div>
+
+<footer class="w3-container w3-teal1 w3-center w3-margin-top">
+
+  <h3 class="text-primary" style="color:#009688" >Find me on social media Links</h3>
+ 
+		<a href="https://www.facebook.com/rajnagarajatp" 		class="col-offset-1 fa fa-facebook-square fa-2x " target="_blank"></a> 		
+		<a href="https://www.instagram.com/?hl=en" style="color:red" 	class="fa fa-instagram col-offset-1 fa-2x"	target="_blank"></a> 
+		<a href="https://twitter.com/7799514314" 	class="fa fa-twitter-square fa-2x col-offset-1  "	target="_blank"> </a> 
+		<a href="https://www.linkedin.com/in/nagaraju-p/"  class="fa fa-linkedin-square fa-2x col-offset-1 " target="_blank"></a> 
+        
+        
+		
+			
+			
+		
+  <p class="text-primary" > Website : <a href="https://www.nagarajup.herokuaoo.com" target="_blank"> https://www.Nagarajup.herokuapp.com</a></p>
+</footer>
+
+</body>
+</html> 
---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<title>Resume</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="images/phot1.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-red" style="text-align:right;">
+            <h2 >Nagaraju P</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Web Developer</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Hyderebad ,India</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>pnagaraju.info@gmail.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>7799514314</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
+          <p>Adobe Photoshop</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>Illustrator</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:85%">85%</div>
+          </div>
+          <p>HTML &amp CSS</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">80%</div>
+          </div> <p>JavaScript &amp JQurey</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b></p>
+          <p>Telugu </p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+          </div>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:75%"></div>
+          </div>
+          <p>Hindu</p>
+          <div class="w3-light-grey w3-round-xlarge ">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:25%  "></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
+        
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Web Developer /cenitithub.com </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Sep 2017 - Current</h6>
+          <p>Initially  i joined as intern cenitithub  designed  some web templates/layout by using Html and Css ,By learn Javascript/JQurey and AngulorJS later they recruted me as WebDeveloper. </p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Lab Assistant/ alits.ac.in/</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>July 2013 - Sep 2014</h6>
+          <p>I have practical work experienced  </p><br>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+       
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Sri Krishnadevaraya Engineering College </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2014 - 2017</h6>
+          <p>Master Degree</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>JNTU Anantapur</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
+          <p>Bachelor Degree</p><br>
+        </div>
+        
+       <div class="w3-container">
+          <h5 class="w3-opacity"><b>ESC Government Polytechnic</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2007 - 2010</h6>
+          <p>Diploma</p><br>
+        </div>
+        
+       <div class="w3-container">
+          <h5 class="w3-opacity"><b>APR School -Lepakshi </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2002 - 2007</h6>
+          <p>School</p><br>
+        </div>
+      </div>
+
+	  <hr>	  
+	  <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Hobbies</h2>
+       
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Playing Cricket </b></h5>
+		 
+		  <h5 class="w3-opacity"><b>Playing Shuttle </b></h5>
+		  <h5 class="w3-opacity"><b>Listing &amp Singing  songs</b></h5>
+		  <h5 class="w3-opacity"><b>Cooking</b></h5>
+		  <h5 class="w3-opacity"><b>Web Designing</b></h5>
+		  <hr>
+		  
+          <hr>
+        </div>
+        
+      </div>
+	  
+
+
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+   
+</div>
+
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <h3>Follow up me on social media Links</h3>
+ 
+	  <a href="https://www.facebook.com/rajnagarajatp" 		target="_blank">
+			<i class=" fa fa-facebook-official w3-hover-opacity btn-md "></i>
+	  </a> 
+    
+		<a href="https://www.instagram.com/?hl=en" 		target="_blank">
+				<i class="fa fa-instagram w3-hover-opacity col-sm-1"></i>
+		</a> 
+
+		  <a href="https://twitter.com/7799514314" 		target="_blank">
+			  <i class="fa fa-twitter w3-hover-opacity"></i>
+		  </a> 
+		  <a href="https://www.linkedin.com/in/nagaraju-p/" target="_blank">
+			  <i class="fa fa-linkedin w3-hover-opacity"></i>
+		  </a> 
+        
+   </div>   
+        
+  <p>Website : <a href="https://www.nagarajforu.ga" target="_blank">Nagarajforu.ga</a></p>
+</footer>
+
+</body>
+</html> 
