536c6793cf14bd8cb95670f1b8dc9dc88d85de6b
Nagaraju p <10nagaraju407@gmail.com>
2018-03-10 21:52:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     
       <div class="w3-white w3-text-grey w3-card-4">
         <div class="w3-display-container">
-          <img src="images/phot1.jpg" style="width:100%" alt="Avatar">
+          <img src="images/phot.png" style="width:100%" alt="Avatar">
           <div class="w3-display-bottomleft w3-container w3-text-red" style="text-align:right;">
             <h2 >Nagaraju P</h2>
           </div>
