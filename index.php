d5b85f68e3e7c5bea8ffc157da366d069de7a310
Nagaraju p <10nagaraju407@gmail.com>
2018-03-11 15:28:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Resume</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="images/favico.png" rel="icon" type="image/x-icon" />
+	<link href="images/favicon.ico" rel="icon" type="image/x-icon" />
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
