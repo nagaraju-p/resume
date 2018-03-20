ca03db754e8d77082fd6aba01dc8fdc57eedd78d
Nagaraju p <10nagaraju407@gmail.com>
2018-03-20 12:56:36 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel='icon' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link href="images/favicon.ico" rel="icon" type="image/x-icon" />
+
 <style>
 		html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
 		.w3-teal1, .w3-hover-teal1:hover {
@@ -28,11 +31,11 @@
       <div class="w3-white w3-text-grey w3-card-4">
         <div class="w3-display-container">
           <img src="images/phot1.jpg" style="width:100%" alt="Avatar">
-          <div class="w3-display-bottomleft w3-container w3-text-red" style="text-align:right;">
-            <h2 >Nagaraju P</h2>
+          <div class="w3-display-bottomright w3-container w3-text-red" style="text-align:right;">
+            <h2 style="color:#fff">Nagaraju P</h2>
           </div>
         </div>
-        <div class="w3-container">
+        <div class="w3-container" style="margin-top:15px" >
           <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Web Developer</p>
           <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Hyderebad ,India</p>
           <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>pnagaraju.info@gmail.com</p>
@@ -128,8 +131,8 @@
       </div>
 
 	  <hr>	  
-	  <div class="w3-container w3-card w3-white">
-        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Hobbies</h2>
+	  <div class="w3-container w3-card w3-white" style="margin-left:0px;margin-right:0px" >
+        <h2 class="w3-text-grey w3-padding-16" style="margin-left:0px;margin-right:0px" ><img src="images/hobbies.png" width="100PX" height="50px" style="margin-left:0px;margin-right:0px">Hobbies</h2>
        
         <div class="w3-container">
           <h5 class="w3-opacity"><b>Playing Cricket </b></h5>
